e23ca61caba4195621c1eb4ed6794a07586bdcb8
ramdevcm <ramdevcm@gmail.com>
2022-05-11 11:04:02 +0530

    marker update

--- static/leaf.js
+++ static/leaf.js
@@ -16,7 +16,19 @@ source.addEventListener('message', function(e){
     tweet = obj.text;
     profileimg=obj.user.profile_image_url_https;
     hashtag = obj.entities.hashtags;
-    marker = L.marker([lat,long],).addTo(mymap).bindPopup('Username: <strong>' + username + '</strong><br>Tweet: <strong>' + tweet + '</strong>');
+
+    
+    var greenIcon = new L.Icon({
+      iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    });
+
+
+    marker = L.marker([lat,long], {icon: greenIcon} ).addTo(mymap).bindPopup('Username: <strong>' + username + '</strong><br>Tweet: <strong>' + tweet + '</strong>');
       
     appendData(username,tweet,profileimg);
 
